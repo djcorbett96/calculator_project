c1af55262962caa0eaf3c650f11f417912cba068
DJ <dcorbett@yext.com>
2021-07-02 16:58:45 -0400

    Additional logic and styling

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ const numButtons = document.querySelectorAll('.num');
 const opButtons = document.querySelectorAll('.operand');
 const equalButton = document.querySelector('#equals');
 const resetButton = document.querySelector('#reset');
+const decimalButton = document.querySelector('#decimal');
+const percentButton = document.querySelector('#percent');
+const negButton = document.querySelector('#negative');
 
 // variables
 let currentOperand;
@@ -51,15 +54,23 @@ numButtons.forEach(item => {
             currentBtnValue = parseInt(currentBtnValue + event.target.value);
         }
         displayScreenNum.innerText = currentBtnValue;
+        opButtons.forEach(item => {
+            item.classList.remove('operand-selected');
+        });
     })
 })
 
 opButtons.forEach(item => {
     item.addEventListener('click', event => {
+        opButtons.forEach(item => {
+            item.classList.remove('operand-selected');
+        });
+        event.target.classList.add('operand-selected');
+        if(currentOperand === undefined) {
+            previousBtnValue = currentBtnValue;
+            currentBtnValue = undefined;
+        }
         currentOperand = event.target.value;
-        displayScreenOp.innerText = currentOperand;
-        previousBtnValue = currentBtnValue;
-        currentBtnValue = undefined;
     })
 })
 
@@ -67,14 +78,19 @@ equalButton.addEventListener('click', ()=> {
     let answer = calculate(currentOperand, previousBtnValue, currentBtnValue);
     currentBtnValue = answer;
     displayScreenNum.innerText = currentBtnValue;
-    displayScreenOp.innerText = '';
+    currentOperand = undefined;
+    opButtons.forEach(item => {
+        item.classList.remove('operand-selected');
+    });
 })
 
 resetButton.addEventListener('click', ()=> {
     currentBtnValue = undefined;
     previousBtnValue = undefined;
-    displayScreenOp.innerText = '';
-    displayScreenNum.innerText = '';
+    displayScreenNum.innerText = 0;
+    opButtons.forEach(item => {
+        item.classList.remove('operand-selected');
+    });
 })
 
 
